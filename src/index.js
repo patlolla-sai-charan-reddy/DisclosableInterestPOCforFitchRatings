87e74662fabd6c3836efcfba709256374b261475
Sai Charan R Patlolla <scrpatlolla@users.noreply.github.com>
2018-09-11 16:01:30 -0400

    structure

--- src/index.js
+++ src/index.js
@@ -5,18 +5,25 @@ import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <p className="legaltext">
+    <div className="App row">
+      <p className="legalText col-lg-12">
         Legal text goes here : Lorem ipsum dolor sit amet, consectetur
         adipiscing elit. In ultrices dictum urna, eu facilisis risus vehicula
-        vel. In pharetra quam eu diam posuere gravida at eu ex. Sed lectus
-        nulla, rhoncus sed velit ullamcorper, pretium dignissim libero. Proin id
-        auctor arcu. Vivamus magna tellus, congue non sem in, sagittis volutpat
-        nibh. In lacinia mauris eu orci consequat efficitur. Vivamus ac
-        venenatis sapien, a egestas purus. Aliquam diam libero, maximus a ex
-        vel, ullamcorper vehicula mi. Sed quis pulvinar ligula. Proin venenatis
-        ante in condimentum commodo. Etiam vitae purus arcu. Nam vel nisi ante.
+        vel.
       </p>
+      <div className="mainContainer col-lg-12">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="col-lg-6">hello</div>
+            <div className="col-lg-6">hello</div>
+          </div>
+
+          <div className="col-lg-12">
+            <div className="col-lg-6">hello</div>
+            <div className="col-lg-6">hello</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
