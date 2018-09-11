fc16a0d4f84614b2fb29e7620ca6e611453ce8cb
Sai Charan R Patlolla <scrpatlolla@users.noreply.github.com>
2018-09-11 15:14:57 -0400

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -6,8 +6,17 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox </h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <p className="legaltext">
+        Legal text goes here : Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. In ultrices dictum urna, eu facilisis risus vehicula
+        vel. In pharetra quam eu diam posuere gravida at eu ex. Sed lectus
+        nulla, rhoncus sed velit ullamcorper, pretium dignissim libero. Proin id
+        auctor arcu. Vivamus magna tellus, congue non sem in, sagittis volutpat
+        nibh. In lacinia mauris eu orci consequat efficitur. Vivamus ac
+        venenatis sapien, a egestas purus. Aliquam diam libero, maximus a ex
+        vel, ullamcorper vehicula mi. Sed quis pulvinar ligula. Proin venenatis
+        ante in condimentum commodo. Etiam vitae purus arcu. Nam vel nisi ante.
+      </p>
     </div>
   );
 }
