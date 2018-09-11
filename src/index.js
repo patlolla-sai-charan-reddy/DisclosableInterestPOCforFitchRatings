81050186bd3e4f63b547f86a830db87b94796d4d
Sai Charan R Patlolla <scrpatlolla@users.noreply.github.com>
2018-09-11 16:28:54 -0400

    setting up the react structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import NonEu from "./non-eu-disqualifying-component";
 
 import "./styles.css";
 
@@ -9,18 +10,37 @@ function App() {
       <p className="legalText col-lg-12">
         Legal text goes here : Lorem ipsum dolor sit amet, consectetur
         adipiscing elit. In ultrices dictum urna, eu facilisis risus vehicula
-        vel.
+        vel. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+        ultrices dictum urna, eu facilisis risus vehicula vel. Lorem ipsum dolor
+        sit amet, consectetur adipiscing elit. In ultrices dictum urna, eu
+        facilisis risus vehicula vel.
       </p>
       <div className="mainContainer col-lg-12">
         <div className="row">
-          <div className="col-lg-12">
-            <div className="col-lg-6">hello</div>
-            <div className="col-lg-6">hello</div>
+          <div className="col-lg-6">
+            <div className="row">
+              <div className="col-lg-12 sec-1">
+                <p>Some Text : 1</p>
+                <NonEu data={"COMPONENT"} />
+              </div>
+              <div className="col-lg-12 sec-2">
+                <p>Some Text : 2</p>
+                <div>COMPONENT</div>
+              </div>
+            </div>
           </div>
-
-          <div className="col-lg-12">
-            <div className="col-lg-6">hello</div>
-            <div className="col-lg-6">hello</div>
+          <hr />
+          <div className="col-lg-6">
+            <div className="row">
+              <div className="col-lg-12 sec-3">
+                <p>Some Text : 3</p>
+                <NonEu data={"COMPONENT"} />
+              </div>
+              <div className="col-lg-12 sec-4">
+                <p>Some Text : 4</p>
+                <div>COMPONENT</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
---
+++ src/non-eu-disqualifying-component.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class NonEu extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>{this.props.data}</p>
+      </div>
+    );
+  }
+}
+
+export default NonEu;
