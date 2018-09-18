cc54b29db789d01c3039b56f5e2785d6b4034737
Sai Charan R Patlolla <scrpatlolla@users.noreply.github.com>
2018-09-18 00:36:31 -0400

    remove the variable

--- src/index.js
+++ src/index.js
@@ -75,5 +75,4 @@ function App() {
   );
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App />, document.getElementById("root"));
